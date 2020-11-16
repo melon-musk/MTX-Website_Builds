908c8ab6cdcae25522a44acbbc09b5e0a94a63bb
Rohan Almeida <rohanalmeida@live.com>
2020-11-16 19:52:08 +0530

    compressed versions of images added

---
+++ precache-manifest.7f76ff9c231247d2fb980a0647791f60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab1b5cd2682c6dd36b161f834d08bc40",
+    "url": "./index.html"
+  },
+  {
+    "revision": "64d07ae9c8b784357fc8",
+    "url": "./static/css/2.e6cf9162.chunk.css"
+  },
+  {
+    "revision": "64d07ae9c8b784357fc8",
+    "url": "./static/js/2.da53b077.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.da53b077.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90bc17c974bd36fe60f3",
+    "url": "./static/js/main.1a772d17.chunk.js"
+  },
+  {
+    "revision": "88a016e7bfd660b206d8",
+    "url": "./static/js/runtime-main.502aa52a.js"
+  }
+]);
